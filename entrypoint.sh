66f9a35dde00a01ec1fe11a3bf2a1d0f0f07cb1c
Kenta Kariya <kentakariya@outlook.com>
2021-04-03 14:09:28 +0200

    Include commit SHA in file name

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 file=$(basename -s .tex $1)
 sha_short=$(echo $GITHUB_SHA | head -c 8)
+repo_name=$(basename $GITHUB_REPOSITORY)
 cd $(dirname $1)
 pdflatex $file.tex
-curl -s -X POST -F "content=$GITHUB_REPOSITORY: PDF file build from $1 for commit #$sha_short" -F "file=@$file.pdf" -o /dev/null $2
+
+pdf_name="${repo_name}_${sha_short}.pdf"
+mv $file.pdf $pdf_name
+curl -s -X POST -F "content=$GITHUB_REPOSITORY: PDF file build from $1 for commit #$sha_short" -F "file=@$pdf_name" -o /dev/null $2
 
