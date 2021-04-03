f361b3e60ac164f497b1d08fca5e4f685b3e113b
Kenta Kariya <kentakariya@outlook.com>
2021-04-03 19:41:29 +0200

    Use latexmk

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,8 @@ file=$(basename -s .tex $1)
 sha_short=$(echo $GITHUB_SHA | head -c 8)
 repo_name=$(basename $GITHUB_REPOSITORY)
 cd $(dirname $1)
-pdflatex $file.tex
+# pdflatex $file.tex
+latexmk -bibtex -pdf -shell-escape $file.tex
 
 pdf_name="${repo_name}_${sha_short}.pdf"
 mv $file.pdf $pdf_name
