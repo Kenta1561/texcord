23dc9c7b9967946aac66ab0a8d46b3b64e36cd72
Kenta Kariya <kentakariya@outlook.com>
2021-04-03 11:15:45 +0200

    Fix variable reference

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@ file=$(basename -s .tex $1)
 sha_short=$(echo $GITHUB_SHA | head -c 8)
 cd $(dirname $1)
 pdflatex $file.tex
-curl -s -X POST -F "content=$GITHUB_REPOSITORY: PDF file build from $1 for commit #$SHA_SHORT" -F "file=@$file.pdf" -o /dev/null $2
+curl -s -X POST -F "content=$GITHUB_REPOSITORY: PDF file build from $1 for commit #$sha_short" -F "file=@$file.pdf" -o /dev/null $2
 
