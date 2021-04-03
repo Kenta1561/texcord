b9bcce936d6da9c3f7ef4f53a494c44c187fc8c1
Kenta Kariya <kentakariya@outlook.com>
2021-04-03 11:12:05 +0200

    Update webhook message

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 file=$(basename -s .tex $1)
+sha_short=$(echo $GITHUB_SHA | head -c 8)
 cd $(dirname $1)
 pdflatex $file.tex
-curl -s -X POST -F "content=File from GitHub Actions" -F "file=@$file.pdf" -o /dev/null $2
+curl -s -X POST -F "content=$GITHUB_REPOSITORY: PDF file build from $1 for commit #$SHA_SHORT" -F "file=@$file.pdf" -o /dev/null $2
 
